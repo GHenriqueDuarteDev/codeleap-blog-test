b8c5198bc3bec0a3d6097540cf0bc6be03581c46
Henrique <ghrduarte@hotmail.com>
2026-03-09 22:06:54 -0300

    feat(codeleap-test): add Button and Input components with styling and functionality

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/Button";
+import { Input } from "../components/ui/Input";
 import { useUserStore } from "../store/useUserStore";
 
 export function SignUp() {
